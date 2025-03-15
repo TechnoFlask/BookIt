2d48fed648eabe965ebeb5352641b3dc145cc3e9
Anish Mukherjee <technoflask03@gmail.com>
2025-03-15 22:29:26 +0530

    feat: landing page(initial)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+import { Button } from "@/components/ui/button"
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs"
+import Link from "next/link"
 
 export default function Home() {
   return (
+    <>
+      <div
+        id="hero"
+        className="w-screen h-[50vh] flex flex-col justify-center items-center gap-10"
+      >
+        <p className="text-6xl text-center">
+          Find the Right Doctor, Book Instantly
+        </p>
+        <p className="text-3xl text-center">
+          Easily schedule appointments with top doctors in just a few clicks. No
+          waiting, no hassle.
+        </p>
+        <Button className="text-xl cursor-pointer px-5 py-8" asChild>
+          <Link href="/dashboard">Book an appointment</Link>
+        </Button>
+      </div>
+    </>
   )
 }
